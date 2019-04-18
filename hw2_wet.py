8792e0572d144ffd7a9c866b7cf233737b4e3a66
Itamar Holder <some_mail@yahani.com>
2019-04-18 18:57:36 +0300

    Merge with snir's changes and clean up

--- hw2_wet.py
+++ hw2_wet.py
@@ -1,35 +1,13 @@
 import numpy as np
 import pandas as pn
 import utils, json, pickle
-import matplotlib as plt
-from sklearn.model_selection import train_test_split
 
-path_to_csv = 'D:\OneDrive\Documents\Degree\Technion\Machine_Learning\hws\HW2'
-
-df = pn.read_csv(r'{}\ElectionsData.csv'.format(path_to_csv), header=0)
+df = pn.read_csv(r'ElectionsData.csv', header=0)
 # train, test = train_test_split(df, test_size=0.1)
 # train, validation = train_test_split(train, test_size=0.25)
 
-# Clean data:
-# features_info_dict = utils.get_features_info_dict(train)
-# for df in [train, validation, test]:
-#     utils.clean_data(df, features_info_dict)
-utils.clean_data(df)
 
+nd_dict = utils.get_features_info_dict(df)
+df = utils.clean_data(df, nd_dict)
 
-# Exploration:
-col = 'Garden_sqr_meter_per_person_in_residancy_area'
-
-utils.feature_hist(df, col, bins=10)
-
-# print(df['Financial_balance_score_(0-1)'].value_counts())
-
-
-
-
-
-
-
-
-
-print('done')
+utils.feature_selection_RFE_test(df)
--- utils.py
+++ utils.py
@@ -1,27 +1,21 @@
 import numpy as np
 import pandas as pn
-
+import category_encoders as ce
 import matplotlib as plt
 
 from sklearn.ensemble import RandomForestClassifier, IsolationForest
 from sklearn import preprocessing
 from sklearn import metrics
-
-
-def split_nominal_feature_to_bool_features(df, feature_to_split):
-    feature_values = df[feature_to_split].unique()
-
-    for issue in feature_values:
-        issue_bool_list = [1 if x == issue else 0 for x in df.Most_Important_Issue]
-        df['{}_{}'.format(feature_to_split, issue)] = issue_bool_list
-
-    df.drop(feature_to_split, axis=1)
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
 
 
 def fill_missing_values(df, features_info_dict):
     features = list(df.columns)
     for feature in features:
         df[feature] = df[feature].apply(lambda v: choose_mean_value(features_info_dict, feature) if str(v) == 'nan' else v)
+    return df
 
 
 def get_features_info_dict(df, eliminate_nd_elements=True):
@@ -65,7 +59,6 @@ def choose_mean_value(features_info_dict, feature_name):
 
 
 def test(x_train, y_train, x_test, y_test, classifier=RandomForestClassifier(n_estimators=3)):
-
     classifier = classifier.fit(x_train, y_train)
 
     # output = forest.predict(test_data_noNaN)
@@ -109,38 +102,86 @@ def find_outliers(df):
     print(count)
 
 
+def feature_selection_RFE_test(df):
+    df_features = df.drop('Vote', axis=1)
+    df_label = df['Vote']
+
+    num_of_all_features = len(df_features.columns)
+    max_features_to_select = int((num_of_all_features / 2) + 4)
+    min_features_to_select = 1
+
+    optimal_features_number, best_score = 0, 0
+
+    print('Starting feature selection test.')
+    print('Total features: {}, max features to select: {}, min features to select: {}.\n'.format(
+                                                                num_of_all_features, max_features_to_select, min_features_to_select))
+
+    for i in range(1, max_features_to_select):
+        x_train, x_test, y_train, y_test = train_test_split(df_features, df_label, test_size=0.3, random_state=0)
+        model = LinearRegression()
+        rfe = RFE(model, n_features_to_select=i)
+        x_train_rfe = rfe.fit_transform(x_train, y_train)
+        x_test_rfe = rfe.transform(x_test)
+        model.fit(x_train_rfe, y_train)
+        score = model.score(x_test_rfe, y_test)
+
+        print('Testing selection of {} features. Score: {}.'.format(i, score))
+
+        if score > best_score:
+            optimal_features_number, best_score = i, score
+
+    print('\nTest Ended.')
+    print('Best choice found: {} features with score: {}.'.format(optimal_features_number, best_score))
+
+
 def clean_data(df, features_info_dict=None):
     ''' Main function to clean data '''
 
-    #  Replacing negative numbers with mean for appropriate columns:
+    # TODO: add "features to drop" section.
+
+    # Replacing negative numbers with mean for appropriate columns:
     features_to_apply = ['Avg_monthly_expense_when_under_age_21']
     replace_negatives_with_mean(df, features_to_apply)
 
-    #  Nominal features splitting:
+    # Convert Vote (label) to unique numbers
+    df['Vote'] = df['Vote'].astype("str")
+    df['Vote'] = df['Vote'].astype("category")
+    df['Vote'] = df['Vote'].cat.rename_categories(range(df['Vote'].nunique())).astype(int)
+    df.loc[df['Vote'].isnull(), 'Vote'] = np.nan  # fix NaN conversion
+
+    # Nominal features splitting:
     nominal_features_to_split = ['Most_Important_Issue', 'Will_vote_only_large_party', 'Main_transportation',
                                  'Occupation']
-    for feature in nominal_features_to_split:
-        split_nominal_feature_to_bool_features(df, feature)
+    df = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True, cols=nominal_features_to_split).fit_transform(df)
 
     if features_info_dict is None:
         features_info_dict = get_features_info_dict(df)
 
     #  Fill missing values by probability/mean:
-    fill_missing_values(df, features_info_dict)
+    df = fill_missing_values(df, features_info_dict)
 
     # Binary nominal (yes/no etc') to numeric (0/1):
-    binray_features_and_values = {'Looking_at_poles_results': {'Yes': 0, 'No': 1},
+    binary_features_and_values = {'Looking_at_poles_results': {'Yes': 0, 'No': 1},
+                                  'Financial_agenda_matters': {'Yes': 0, 'No': 1},
                                   'Married': {'Yes': 0, 'No': 1},
                                   'Gender': {'Female': 0, 'Male': 1},
                                   'Voting_Time': {'By_16:00': 0, 'After_16:00': 1},
                                   'Age_group': {'Below_30': 0, '30-45': 1, '45_and_up': 2}}
-    for feature, replacement_dict in binray_features_and_values.items():
+    for feature, replacement_dict in binary_features_and_values.items():
         df[feature] = df[feature].map(replacement_dict)
 
     #  Normalization phase:
-    features_to_normalize_0_to_1 = []
+    features_to_normalize_0_to_1 = ['Age_group', 'Avg_Residancy_Altitude', 'Avg_size_per_room',
+                                    'Garden_sqr_meter_per_person_in_residancy_area', 'Num_of_kids_born_last_10_years',
+                                    'Number_of_differnt_parties_voted_for', 'Number_of_valued_Kneset_members',
+                                    'Phone_minutes_10_years']
     features_to_normalize_minus1_to_1 = ['AVG_lottary_expanses', 'Avg_monthly_expense_on_pets_or_plants',
-                                         'Occupation_Satisfaction', 'Avg_environmental_importance']
+                                         'Occupation_Satisfaction', 'Avg_environmental_importance',
+                                         'Avg_Satisfaction_with_previous_vote', 'Avg_education_importance',
+                                         'Avg_government_satisfaction', 'Avg_monthly_expense_when_under_age_21',
+                                         'Avg_monthly_household_cost', 'Avg_monthly_income_all_years',
+                                         'Last_school_grades', 'Overall_happiness_score', 'Political_interest_Total_Score',
+                                         'Weighted_education_rank', 'Yearly_ExpensesK', 'Yearly_IncomeK']
     scalar_0_to_1 = preprocessing.MinMaxScaler(feature_range=(0, 1))
     scalar_minus1_to_1 = preprocessing.MinMaxScaler(feature_range=(-1, 1))
 
@@ -150,4 +191,4 @@ def clean_data(df, features_info_dict=None):
     for feature in features_to_normalize_minus1_to_1:
         df[feature] = scalar_minus1_to_1.fit_transform(df[feature].values.reshape(-1, 1))
 
-
+    return df
